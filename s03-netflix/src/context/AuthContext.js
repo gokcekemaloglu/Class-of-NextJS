168c670715d27f3b4a2bdb32100dc8a2a4aacfda
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-02 13:55:55 +0300

    feat: updated login page and auth context with new functionality

--- s03-netflix/src/context/AuthContext.js
+++ s03-netflix/src/context/AuthContext.js
@@ -1,9 +1,10 @@
 "use client"
 
-import { toastSuccessNotify } from "@/helpers/ToastNotify"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { toastErrorNotify, toastSuccessNotify } from "@/helpers/ToastNotify"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { createContext, useContext } from "react"
 import { auth } from "@/auth/firebase";
+import { useRouter } from "next/navigation";
 
 // auth(yetki) işlemlerini yaptığımız context
 export const YetkiContext = createContext()
@@ -14,16 +15,28 @@ export const useAuthContext = () => {
 }
 
 const AuthContextProvider = ({children}) => {
+    const router = useRouter()
 
     const createKullanici = async (email, password, displayName) => {
         try {
             await createUserWithEmailAndPassword(auth, email, password)
-            // toastSuccessNotify("")
+            toastSuccessNotify("Register success")
+            router.push("/login")
         } catch (error) {
-            
+            toastErrorNotify(error.message)
         }
     }
 
-    return <YetkiContext.Provider value={{createKullanici}}> {children} </YetkiContext.Provider>
+    const signIn = async (email, password) => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            toastSuccessNotify("Login success")
+            router.push("/profile")
+        } catch (error) {
+            toastErrorNotify(error.message)
+        }
+    }
+
+    return <YetkiContext.Provider value={{createKullanici, signIn}}> {children} </YetkiContext.Provider>
 }
 export default AuthContextProvider
