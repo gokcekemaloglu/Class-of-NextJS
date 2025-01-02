ad76cf5866bed5d7cd5a597261e1c33fb6030ad9
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-02 11:30:52 +0300

    Added ToastContainer to layout.js

--- s03-netflix/src/app/layout.js
+++ s03-netflix/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
           {children}
-       
+        <ToastContainer/>
       </body>
     </html>
   );
