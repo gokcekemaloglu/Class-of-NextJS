f834949d4a7da15235bc49af071e25718a3b1ad5
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-02 10:08:12 +0300

    Added metadata to pages for SEO purposes

--- class-notes/s01-intro_and_routing/src/components/Navbar.jsx
+++ class-notes/s01-intro_and_routing/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 // import logo from "/public/clarusway-logo.png";
 
@@ -20,6 +21,8 @@ const navigation = [
 ];
 
 const Navbar = () => {
+  let pathname = usePathname();
+
   return (
     <nav className="bg-navbarColor text-sm px-4 flex justify-between">
       <div className="flex items-center">
@@ -27,12 +30,6 @@ const Navbar = () => {
           <Link href="/" className="flex items-center">
             {/* //? Next.js, import edilen dosyaya göre image genişliğini ve yüksekliğini otomatik olarak belirler ancak Next.js'nin build işlemi sırasında remote dosyalara erişimi olmadığından, genişlik ve yükseklik özelliklerini manuel olarak sağlamanız gerekir. */}
             {/* //* farklı domainlerden alınan image'ler için ilgili domainler config dosyasında belirtilmelidir. */}
-
-            {/* <img
-              src="https://clarusway.com/wp-content/uploads/2022/12/clarusway-logo-black.png"
-              alt="logo"
-              width={150}
-            /> */}
             <Image
               width={150}
               height={50}
@@ -47,7 +44,9 @@ const Navbar = () => {
           {navigation.map((item) => (
             <li
               key={item.title}
-              className={`font-medium hover:bg-gray-300 rounded-full py-2 px-4 hover:text-white inline-block text-lg `}
+              className={`font-medium hover:bg-gray-300 rounded-full py-2 px-4 hover:text-white inline-block text-lg ${
+                item.path === pathname && "bg-gray-300"
+              } `}
             >
               {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
               <Link href={item.path}> {item.title} </Link>
